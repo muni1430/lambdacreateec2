23d1d7adef5367ddcb66343cc904037f9fcce9bb
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-17 19:33:43 +0530

    Create createec2lambda.tf

---
+++ createec2lambda.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region="us-east-1"
+}
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function.zip"
+  function_name = "new_one_lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+ source_code_hash = filebase64sha256("lambda_function.zip")
+runtime = "Python 3.9"
+}
